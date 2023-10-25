f9f39dc15685631c25dd3ff1136c77e46ffd71f2
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-10-25 09:54:21 +0000

    Vet Clinic DATABASE query and update animals table

--- queries.sql
+++ queries.sql
@@ -25,3 +25,42 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3; 
+
+-- Begin Transaction 
+BEGIN; 
+UPDATE animals SET species = 'unspecified' ; 
+select name, species from animals; 
+ROLLBACK;
+
+-- Begin Transaction 
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT; 
+SELECT * FROM animals; 
+
+-- Begin Transaction 
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK; 
+SELECT * FROM animals; 
+
+-- Begin the transaction
+BEGIN; 
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint; 
+UPDATE animals SET weight_kg = weight_kg * -1; 
+SELECT * FROM animals;
+ROLLBACK TO my_savepoint; 
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
+SELECT * FROM animals;
+COMMIT;  
+
+SELECT COUNT(*) FROM animals ;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals; 
+SELECT neutered, MAX(escape_attempts) AS max_escape_attempts FROM animals GROUP BY neutered; 
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species; 
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species; 
--- schema.sql
+++ schema.sql
@@ -10,3 +10,6 @@ CREATE TABLE animals (
     weight_kg DECIMAL(5,2), 
     PRIMARY KEY(id)
 );
+
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(255);
